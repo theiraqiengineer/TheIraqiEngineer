775cae54dce119d02fadfbf9afbda1b860353817
sluvd <salbuhayder@lucidbots.com>
2025-04-15 01:07:28 +0200

    x

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Frontend server running on http://localhost:${PORT}`);
+}); 
